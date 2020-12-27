97949cad2d4f087cae35e3a1dc034ee9482b9786
@hmed <spb6926@gmail.com>
2020-12-27 21:27:05 +0100

    add buttons

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import Counters from "./Counters";
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(1)
     const plus=()=>{
-        setCount(count+1);
+        setCount(count+2);
     }
     const minus=()=>{
         setCount(count-1)
     }
+    const prod=()=>{
+        setCount(count*2)
+    }
+    const del=()=>{
+        setCount(count/2)
+    }
+    const clr=()=>{
+        setCount(0)
+    }
     return (
         <div align='center'>
             <h1>Calculator</h1>
-            <Counters count={count} plus={plus} minus={minus}/>
+            <Counters count={count} plus={plus} minus={minus} prod={prod} del={del} clr={clr}/>
         </div>
     );
 }
--- src/Counters.js
+++ src/Counters.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export default function Counters(props) {
-    const{count,plus,minus}=props
+    const{count,plus,minus,prod,del,clr}=props
     return (
         <div>
+            <button onClick={del}>/</button>
             <button onClick={minus}>-</button>
             {count}
             <button onClick={plus}>+</button>
+            <button onClick={prod}>*</button>
+            <button onClick={clr}>clear</button>
         </div>
     );
 }
